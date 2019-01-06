928befefb2494fb78d83348defa6dccfcc01ad9b
Anantha <Anantha.kumar.u@nl.abnamro.com>
2019-01-06 11:53:14 +0100

    Java doc

--- GliefService/src/main/java/com/boot/glief/service/GliefServiceApplication.java
+++ GliefService/src/main/java/com/boot/glief/service/GliefServiceApplication.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GliefServiceApplication {
 
+	/*
+	Spring boot start application
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(GliefServiceApplication.class, args);
 	}
