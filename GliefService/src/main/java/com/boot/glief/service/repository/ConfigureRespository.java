5eeebffecf4ff25ab6e0432e271790ec2294965f
Anantha <Anantha.kumar.u@nl.abnamro.com>
2019-01-30 00:22:17 +0100

    Java doc

---
+++ GliefService/src/main/java/com/boot/glief/service/repository/ConfigureRespository.java
@@ -0,0 +1,7 @@
+package com.boot.glief.service.repository;
+
+import com.boot.glief.service.model.Configure;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConfigureRespository extends JpaRepository<Configure, Integer> {
+}
